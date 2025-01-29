4ff87f54468f9cd68ca976db9d9aa5c8494f08cc
Joel Anderson <172464269+HonkyTonkRobot@users.noreply.github.com>
2025-01-30 11:42:51 +1300

    add confetti to blog

---
+++ scripts/confetti.js
@@ -0,0 +1,5 @@
+//bonus confetti
+import confetti from 'https://cdn.skypack.dev/canvas-confetti'
+// ... or we could use one of the other services
+//   import confetti from 'https://esm.run/canvas-confetti'
+confetti()
